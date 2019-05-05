2cf218bd39da11b4c090e8e8b999696c5ddb2f23
Sdevem <rs.sdevem@gmail.com>
2019-05-05 00:37:37 -0300

    repository e service para categoria

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -3,29 +3,28 @@ package com.nelioalves.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 
 public class CategoriaResource {
+	@Autowired
+	private CategoriaService service;
 
-	@RequestMapping(method = RequestMethod.GET)
-
-	public List listar() {
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id){
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 		
-		Categoria categoria1 = new Categoria (1, "Informatica");
-		Categoria categoria2 = new Categoria (1, "Escritorio");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(categoria1);
-		lista.add(categoria2);
-		
-		return lista;
 	}
 
 }
