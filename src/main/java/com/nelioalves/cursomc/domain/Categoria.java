55f81497d43362626153580271b04f7721b49e61
Sdevem <rs.sdevem@gmail.com>
2019-05-04 23:20:10 -0300

    Testando a Primeira Classe de Dominio

---
+++ src/main/java/com/nelioalves/cursomc/domain/Categoria.java
@@ -0,0 +1,59 @@
+package com.nelioalves.cursomc.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable  {
+	private static final long serialVersionUID = 1L;
+	
+	private int id;
+	private String nome;
+	
+	public Categoria() {
+	
+	}
+
+	public Categoria(int id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
+}
--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.nelioalves.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -11,8 +16,16 @@ public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
 
-	public String listar() {
-		return "Rest esta funcionando";
+	public List listar() {
+		
+		Categoria categoria1 = new Categoria (1, "Informatica");
+		Categoria categoria2 = new Categoria (1, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(categoria1);
+		lista.add(categoria2);
+		
+		return lista;
 	}
 
 }
